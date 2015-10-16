3c55f5b1b11365c1839bd9fc35010b058fa0a832
yamadapc <tacla.yamada@gmail.com>
2015-10-16 19:43:03 -0300

    Lint node_lp JavaScript

--- node_lp/app.js
+++ node_lp/app.js
@@ -1,51 +1,48 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 */
-'use strict'
+'use strict';
 
-let
-  _ = require('lodash')
-  , fs = require('fs')
+let fs = require('fs')
   , express = require('express')
   , conf = require('./conf')
   , log = require('./lib/log')
-  , Client = require('./lib/client')
-  , exphbs = require('express-handlebars')
+  , exphbs = require('express-handlebars');
 
-let app = express()
+let app = express();
 
 app.engine('hbs',exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
-}))
+}));
 
-app.set('view engine','hbs')
+app.set('view engine','hbs');
 
-app.use('/public',express.static('./public'))
+app.use('/public',express.static('./public'));
 
-app.use(require('body-parser').urlencoded({extended:false}))
+app.use(require('body-parser').urlencoded({extended:false}));
 
 app.use((req,res,next) => {
-  res.locals.conf = conf
-  next()
-})
+  res.locals.conf = conf;
+  next();
+});
 
 // cookie session
 app.use(require('express-session')({
   secret: 'session_secret',
   saveUninitialized: true,
   resave: false,
-}))
+}));
 
-app.listen(conf.port)
-log(`:${conf.port}`)
+app.listen(conf.port);
+log(`:${conf.port}`);
 
 for(let file of fs.readdirSync('./routes')){
   if(file.endsWith('.js')){
-    app.use('/',require(`./routes/${file}`))
+    app.use('/',require(`./routes/${file}`));
   }
 }
 
 // handle errors
-app.use((err,req,res,next) => {
-  res.json({error:err.toString()})
-})
+app.use((err, req, res, next) => {
+  res.json({error:err.toString()});
+});
 
