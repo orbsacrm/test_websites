4e939d4e7a580ead2fed703bc7e60da10a02d026
Erich Oliveira <ericholiveira10@gmail.com>
2015-10-19 22:44:58 -0200

    php lp implementation

---
+++ php_lp/index.php
@@ -0,0 +1,142 @@
+<?php /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 */
+/* class.billing.php */
+require_once(__DIR__.'/deps/config.php');
+require_once(__DIR__.'/deps/class.billing.php');
+
+session_start();
+
+$b = new Billing($config);
+# page request
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    session_unset();
+    session_destroy();
+    $offer = $b->offer_view($config['offer'])['result'];
+    $cycle = $offer['cycle'];
+
+}
+
+# form post
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    # create a new billing instance
+
+
+    $form = $_POST;
+
+    # attempt to create the customer
+    $res = $b->customer_create($form);
+
+    # successfully created the customer
+    if($res['ok']){
+
+        # set the customer id into the session
+        $_SESSION['customer'] = $res['result'];
+
+        # goto the payment page
+        header('location: payment.php');
+
+    }else{
+        # set errors to show on the page, something was wrong
+        $errors = $res['errors'];
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Landing page - Orbsa Widgets Company</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/public/main.css">
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+    <div class="col-sm-12">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="text-center">
+                    <img src="public/symbol.svg" class="symbol-logo" title="Orbsa Widgets Company">
+                </h3>
+                <hr>
+                <?php
+                # show errors if they existed with customer creation
+                if(!empty($errors)){
+                    echo "<p>";
+                    foreach($errors as $str){
+                        echo "<span style='color:crimson'>$str</span><br>";
+                    }
+                    echo "</p>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-7">
+                <h3><?=$offer['name']?></h3>
+                <p><?=$offer['description']?></p>
+                <hr>
+                <p>
+                <ul class="text-muted tiny">
+                    <li>Initial method: <?=$cycle['initial_method']?></li>
+                    <li>Initial amount: <?=$cycle['initial_amount_dollars']?></li>
+                    <li>Trial period: <?=$cycle['trial_seconds_human']?></li>
+                    <li>Cycle price: <?=$cycle['cycle_amount_dollars']?> every <?=$cycle['cycle_seconds_human']?></li>
+                </ul>
+                </p>
+            </div>
+
+            <div class="col-md-5">
+                <input type="hidden" id="flash">
+                <form class="form well" id="lead-form" method="post" action="">
+                    <h3 class="form-signin-heading">Register now!</h3>
+                    <p>Where should we send <em>your first package?</em></p>
+
+                    <label class="sr-only">First name</label>
+                    <input name="first" type="text" class="form-control" placeholder="First name" required autofocus>
+
+                    <label class="sr-only">Last name</label>
+                    <input name="last" type="text" class="form-control" placeholder="Last name" required>
+
+                    <label class="sr-only">Address</label>
+                    <input name="address" type="text" class="form-control" placeholder="Address" required>
+
+                    <label class="sr-only">City</label>
+                    <input name="city" type="text" class="form-control" placeholder="City" required>
+
+                    <label class="sr-only">State</label>
+                    <select name="state" class="form-control" placeholder="State" required></select>
+
+                    <label class="sr-only">Country</label>
+                    <select name="country" class="form-control" placeholder="Country" required></select>
+
+                    <label class="sr-only">Zipcode</label>
+                    <input name="zipcode" type="text" class="form-control" placeholder="Zipcode" required>
+
+                    <label class="sr-only">Email address</label>
+                    <input name="email" type="email" class="form-control" placeholder="Email address" required>
+
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">
+                        Continue
+                    </button>
+                </form>
+                <p class="text-right">
+                    <a href="#" class="tiny" id="fill-lead">(fill form)</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="//localhost:8081/v1/js?fresh=1"></script>
+<script src="/public/main.js"></script>
+</body>
+</html>
+
---
+++ php_lp/thanks.php
@@ -0,0 +1,124 @@
+<?php /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 */
+require_once(__DIR__.'/deps/config.php');
+require_once(__DIR__.'/deps/class.billing.php');
+
+session_start();
+
+if(empty($_SESSION['customer'])){
+    header('location: index.php');
+    die;
+}
+
+$b = new Billing($config);
+
+$offer_id = $config['offer'];
+$offer = $b->offer_view($offer_id)['result'];
+$cycle = $offer['cycle'];
+$customer = $b->customer_view($_SESSION['customer'])['result'];
+$upsell_done = false;
+$upsell_done_error = false;
+if($config['upsell']['enabled']){
+    $upsell = $b->offer_view($config['upsell']['offer'])['result'];
+}
+
+# form post
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $res = $b->offer_purchase($_SESSION['customer'],$offer_id,[]);
+    if($res['ok']){
+        # goto the payment page
+        $upsell_done=true;
+    }else{
+        # set errors to show on the page, something was wrong
+        $errors = $res['errors'];
+        $upsell_done_error = true;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Landing page - Orbsa Widgets Company</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/public/main.css">
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+    <div class="col-sm-12">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="text-center">
+                    <img src="public/symbol.svg" class="symbol-logo" title="Orbsa Widgets Company">
+                </h3>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 text-center">
+                <h3><i class="fa fa-check"></i> Thanks <?=$customer['details']['first']?>!</h3>
+                <p>
+                    Your order was placed with Orbsa Widgets Company.
+                </p>
+
+                <?php if($config['upsell']['enabled']) : ?>
+                <div id="upsell-sorry" style="<?=$upsell_done_error ? '':'display:none'?>">
+                    <hr>
+                    <h3><i class="fa fa-thumbs-down"></i> Sorry <?=$customer['details']['first']?>..</h3>
+                    <p>
+                        It looks like there was a problem with the payment, try contacting us to get it sorted out.
+                        Anyway, thanks for the purchase and we hope you enjoy your products!
+                    </p>
+                </div>
+                <div id="upsell-thanks" style="<?=$upsell_done ? '':'display:none'?>">
+                    <hr>
+                    <h4><i class="fa fa-check"></i> Thanks again!</h4>
+                    <p>
+                        Your upsell order was placed as well, we hope you enjoy your products.
+                    </p>
+                </div>
+                <div id="upsell-offer" style="<?=$upsell_done ? 'display:none':''?>">
+                    <br>
+                    <div class="text-left well">
+                        <p>
+                            <?=$customer['details']['first']?>, did you also want to take advantage of this additional deal that
+                            we only offer to subscription member customers?
+                        </p>
+                        <hr>
+                        <h4><?=$upsell['name']?></h4>
+                        <p><?=$upsell['description']?></p>
+                        <p class="text-center">
+                            <br>
+                            <form method="post" action="">
+                            <button class="btn btn-success btn-lg" id="accept-upsell" type="submit">
+                                <i class="fa fa-plus"></i> Add it to my order <?=$upsell['price_human']?>
+                            </button>
+
+                            <button class="btn btn-lg" id="deny-upsell">
+                                No thanks
+                            </button>
+                            </form>
+                        </p>
+                    </div>
+                </div>
+                <?php endif; ?>
+
+                <hr>
+                <p><a href="/" class="tiny">(clear session and return to start)</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="//localhost:8081/v1/js?fresh=1"></script>
+<script src="/public/main.js"></script>
+</body>
+</html>
+
