3c9415f9416408537f548d1171066e7f414c591b
taky <taky@taky.com>
2015-10-26 20:05:03 -0400

    delete everything

--- php_example/deps/config.php
+++
@@ -1,9 +0,0 @@
-<?php /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 */
-/* config.php */
-
-$config = [
-  'url' => 'http://127.0.0.1:8081',
-  'auth' => false,
-  'key' => '7z68p0ts3iohxc5v',
-];
-
